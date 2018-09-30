dc8331dcf5067f3a7c8c90f965ca948c0f24b5f3
[JZY11] <[1291813139@qq.com]>
2018-09-30 13:06:34 +0800

    update

--- mypython07.py
+++ mypython07.py
@@ -4,13 +4,36 @@ for循环和可迭代对象遍历：
     for  变量  in  可迭代对象：
         循环体语句
 
+    python包含以下几种可迭代对象：
+        1、序列。 包含：字符串、列表、元组
+        2、字典
+        3、迭代器对象（iterator）
+        4、生成器函数（generator）
 '''
 
-# 如：遍历一个元组或列表
-for x in range(20, 30, 40):
+# 如：遍历一个元组
+for x in (20, 30, 40):
     print(x)
 
+# 遍历一个列表
+for x in [20, 30, 40]:
+    print(x*3)
 
+# 遍历一个字符串
+for y in "abcdefg":
+    print(y)
 
+# 遍历一个字典
+d = {'name':'gaoqi','age':18,'address':'西二旗地铁站'}
+for x in d: # 遍历字典中所有的key
+    print(x)
 
+for x in d.keys():  # 遍历字典中所有的key
+    print(x)
+
+for x in d.values():    # 遍历字典中所有的value
+    print(x)
+
+for x in d.items(): # 遍历字典中所有的“键值对”
+    print(x)
 
