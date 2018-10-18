fbb40e578da12309ee63f91fc2c04e8f9b9b3103
[JZY11] <1291813139@qq.com>
2018-10-18 11:05:31 +0800

    update

---
+++ myoop14.py
@@ -0,0 +1,13 @@
+# 测试super() ，代表的是父类的定义，而不是父类的对象
+
+class A:
+    def say(self):
+        print("A:",self)
+
+class B(A):
+    def say(self):
+        # A.say(self)
+        super().say()
+        print("B:",self)
+
+B().say()
