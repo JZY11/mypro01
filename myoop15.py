a8cf6fe843bd24b0d05a6e1da4b2cac1f61581e1
[JZY11] <1291813139@qq.com>
2018-10-22 10:18:01 +0800

    update

---
+++ myoop15.py
@@ -0,0 +1,27 @@
+# 多态（方法进行了不同的实现）
+
+class Man:
+    def eat(self):
+        print("饿了，开始吃饭了")
+
+class Chinese(Man):
+    def eat(self):
+        print("中国人用筷子吃饭")
+
+class English(Man):
+    def eat(self):
+        print("英国人用刀叉吃饭")
+
+class Indian(Man):
+    def eat(self):
+        print("印度人用右手吃饭")
+
+
+def manEat(m):
+    if isinstance(m,Man):
+        m.eat()         # 多态，一个方法调用，根据对象不同(传入的对象)调用不同的方法！
+    else:
+        print("不能吃饭！")
+
+manEat(Chinese())       # 参数要传进来一个对象实例
+manEat(English())
