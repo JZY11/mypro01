5cb8c7e51e9aa0ab49c307fe657d34386f559662
[JZY11] <1291813139@qq.com>
2018-10-25 09:51:07 +0800

    update

--- myoop20.py
+++ myoop20.py
@@ -1,3 +1,5 @@
+# 测试 has-a关系，使用组合   注意：is-a关系使用“继承”，从而实现子类拥有父类的属性和方法，它指代的是类似这样的关系：狗是动物  狗类就继承了动物类
+# has-a关系，我们可以使用组合，也能实现一个类拥有另外一个类的属性和方法，has-a关系指代的是这样的关系：手机拥有CPU， MobilePhone has CPU
 
 class MobilePhone:
     def __init__(self,cpu,screen):
